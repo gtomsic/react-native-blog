ed7efd203189e75d0ec30439be55b9bc5be90e30
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-06-12 08:55:40 -0600

    Delete blogpost complete

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,19 +1,35 @@
-import { StyleSheet, Text, View, FlatList, Button } from "react-native";
+import {
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    Button,
+    TouchableOpacity,
+} from "react-native";
 import React, { useContext } from "react";
+import { Feather } from "@expo/vector-icons";
 
 import { Context } from "../context/BlogContext";
 
 const IndexScreen = () => {
-    const { state, addBlogPost } = useContext(Context);
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context);
     return (
         <View>
-            <Text>IndexScreen</Text>
             <Button onPress={addBlogPost} title="Add Post" />
             <FlatList
                 data={state}
-                keyExtractor={(blog) => blog.title}
+                keyExtractor={(blog) => blog.id}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>;
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <TouchableOpacity
+                                onPress={() => deleteBlogPost(item.id)}
+                            >
+                                <Feather style={styles.icon} name="trash" />
+                            </TouchableOpacity>
+                        </View>
+                    );
                 }}
             />
         </View>
@@ -22,4 +38,19 @@ const IndexScreen = () => {
 
 export default IndexScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        borderColor: "#dedede",
+        paddingHorizontal: 15,
+    },
+    title: {
+        fontSize: 18,
+    },
+    icon: {
+        fontSize: 24,
+    },
+});
